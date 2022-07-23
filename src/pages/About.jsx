4aa56fa07f16d62f85c72a195da3f4f63d07c2a1
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2022-07-24 01:56:44 +0800

    Update: About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,32 +5,20 @@ import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 // import Contact from '../components/Contact';
 
 const About = () => {
-  // const { t } = useTranslation(["about"]);
+  const { t } = useTranslation(["about"]);
+  // const { t, i18n } = useTranslation(["navbar"]);
+	// const handleLangChange = e => {
+	// 	i18n.changeLanguage(e.target.id);
+	// };
 
-  const { t, i18n } = useTranslation(["navbar"]);
-	const handleLangChange = e => {
-		i18n.changeLanguage(e.target.id);
-	};
-
-  // const originalHeading = `${t("aboutMe")}`;
   const originalHeading = <ArrowBackIosNewIcon />;
-  const updatedHeading = `${t("aboutMe1")}`;
+  const updatedHeading = `${t("arrow")}`;
   const [heading, setHeading] = useState(originalHeading);
-  const handleHeading = () => {
-    setHeading(`${t("aboutMe")}`);
-  };
 
   return (
     <>
     <main id="about-me" name="about-me">
       <Link to="/" className="arrow-back slideUp">
-        {/* <span>
-          <ArrowBackIosNewIcon 
-            to="/" 
-            onMouseOver={() => setHeading(updatedHeading)}
-            onMouseLeave={() => setHeading(originalHeading)}
-          />
-        </span> */}
         <span
           to="/" 
           onMouseOver={() => setHeading(updatedHeading)}
@@ -38,15 +26,6 @@ const About = () => {
         > {heading}
         </span>
       </Link>
-
-
-  
-
-
-
-
-
-
       <h1 className="slideUp titles menu-4">
         <span>
           <Trans i18nKey="titles.title-1">
@@ -63,24 +42,24 @@ const About = () => {
       </h1>
 
       <div className="about-me">
+        <h2>About Me</h2>
+        {/* <h1>About Me</h1> */}
         <p>
-          Hello, my name is <i><strong>Jasmine Zeng</strong></i>. I am a full-stack web developer, a language enthusiast and a marketing undergraduate from Zhongshan, Mainland China, currently based in Hong Kong. As a person who values personal development to a great extent, I constantly strive to learn new skills and aim for proficiency.
+          Hello, my name is <i><strong>Jasmine Zeng</strong></i>. 
+          
+          
+          
+          I am a Hong-Kong-based full-stack web developer, a marketing undergraduate and a language enthusiast from Zhongshan, Mainland China. 
+          
+          
+          As a person who values personal development to a great extent, I constantly strive to learn new skills and aim for proficiency. My interests extend beyond academic studies to web development, language learning, hiking, history and many more.
         </p>
         <p>
-          My interests extend beyond academic studies to web development, learning languages, hiking and traveling. 
-          
-          
-          
-          
           My first foray into it was in late 2021, when I wanted to create a blog showcasing my hiking experiences and photos. I quickly realized that building websites became a passion for me, as I am always eager to learn more so as to develop new features. Therefore, I fully committed myself into web development in summer 2022 by joining a bootcamp, acquiring languages, frameworks and other tools like HTML, CSS, JavaScript, Node.js, Jest, React, Redux Toolkit, PostgreSQL and Redis. Although there were countless challenges along the way, I truly enjoyed the learning experience. 
         </p>
         <p>
           Furthermore, I am an avid language devotee. A language acts like a bridge that connects me to people and their cultures on a deeper level. For this reason, I find it extremely fascinating. Right now, I am actively learning German and French after being fluent in English and Cantonese. 
         </p>
-
-
-
-
         <p>
           Besides the above hobbies, I also enjoy hiking, visiting historical and art museums, and exploring local neighborhoods for their food and cultures. If you are intrigued by my discoveries, please visit my <a href="https://deinelieblingsjasmin.com" target="_blank">blog</a>. 
         </p>
