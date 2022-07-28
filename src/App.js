b8aba1f0ecc9d9c82c64c0fefe181343021d98b9
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2022-07-28 09:54:36 +0800

    Create: Contact.jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // components
 import Navbar from './components/Navbar';
 import About from './pages/About';
-import Contact from './components/Contact';
+// import Contact from './components/Contact';
+import OnProgress from './pages/OnProgress';
 // styles
 import { Spinner } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -17,7 +18,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Navbar />} />
             <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
+            <Route path="contact" element={<OnProgress />} />
           </Routes>
         </Suspense>
       </Router>
---
+++ src/pages/OnProgress.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useTranslation, Trans } from 'react-i18next';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+
+const OnProgress = () => {
+  const { t } = useTranslation(["contact"]);
+
+  const originalArrow = <ArrowBackIosNewIcon />;
+  const updatedArrow = `${t("arrow")}`;
+  const [arrow, setArrow] = useState(originalArrow);
+
+  return (
+    <>
+    <div className="on-progress-bg slideUp">
+      <Link to="/" className="arrow-back slideUp">
+        <span
+          to="/" 
+          onMouseOver={() => setArrow(updatedArrow)}
+          onMouseLeave={() => setArrow(originalArrow)}
+        > 
+          {arrow}
+        </span>
+      </Link>
+      <span>
+        <h1 className="msg">
+          {t("msg.msg-1")} <br />
+          {t("msg.msg-2")}
+        </h1>
+      </span>
+    </div>
+    </>
+  )
+};
+
+export default OnProgress;
