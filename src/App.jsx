a498f7b55bc987f3a87a345254f57f0475af535a
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2024-06-13 20:57:43 +0800

    Add: Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
 import { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// Googel Analytics on testing
-// import ReactGA from 'react-ga4';
-
-// ReactGA.initialize("G-RWQCJMQ1X1");
-// ReactGA.send({ hitType: "pageview", page: "/my-path", title: "Custom Title" });
-// ReactGA.event({
-//   category: "your category",
-//   action: "your action",
-//   label: "your label", // optional
-//   value: 99, // optional, must be a number
-//   nonInteraction: true, // optional, true/false
-//   transport: "xhr", // optional, beacon/xhr/image
-// });
-
 // components
 import Home from './pages/Home';
 import About from './pages/About';
-// import Contact from './components/Contact';
 import OnProgress from './pages/OnProgress';
 // styles
 import { Spinner } from 'react-bootstrap';
